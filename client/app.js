c48d6b5e67bfefc670010ebecab517f165fc33cd
AafaqAhmed-074 <145212595+AafaqAhmed-074@users.noreply.github.com>
2026-05-21 11:07:32 +0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -27,8 +27,56 @@ async function getStudents() {
     list.innerHTML = '';
 
     data.forEach(student => {
+
         const li = document.createElement('li');
-        li.textContent = `${student.name} - ${student.age}`;
+
+        li.innerHTML = `
+            ${student.name} - ${student.age}
+
+            <button onclick="deleteStudent('${student._id}')">
+                Delete
+            </button>
+
+            <button onclick="updateStudent('${student._id}')">
+                Update
+            </button>
+        `;
+
         list.appendChild(li);
     });
+}
+
+async function deleteStudent(id) {
+
+    await fetch(`http://localhost:5000/students/${id}`, {
+        method: 'DELETE'
+    });
+
+    alert('Student Deleted');
+
+    getStudents();
+}
+
+async function updateStudent(id) {
+
+    const newName = prompt('Enter new name');
+    const newAge = prompt('Enter new age');
+
+    await fetch(`http://localhost:5000/students/${id}`, {
+
+        method: 'PUT',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            name: newName,
+            age: newAge
+        })
+    });
+
+    alert('Student Updated');
+
+    getStudents();
 }
